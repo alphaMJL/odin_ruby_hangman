f0aca15efe7bce00972301fff95f7a7227d70b15
alphaMJL <alphaMJL@github>
2023-10-04 12:15:20 -0400

    added opening screen and input validation. added entry into game loop

--- hangman.rb
+++ hangman.rb
@@ -2,6 +2,52 @@ require_relative 'lib/game_logic'
 require_relative 'lib/input_validation'
 require_relative 'lib/save_and_load'
 
+def start_screen
+  opening = <<-OPENING
+  Welcome to Hangman. In this game you will get a random word
+  of 5-12 characters. You will start with 10 guesses. You must
+  uncover the word before your guesses run out. A correct guess
+  will not consume a guess. 
+
+  Type 'start' to start a new game.
+  Type 'load' to load a saved game.
+  Type 'exit' to exit game.
+
+  OPENING
+  puts opening
+end
+
+def select_mode
+  InputValidation.opening_inputs(gets.chomp)
+end
+
+def start_game
+  start_screen
+  selection = select_mode
+  case selection
+  when 'start'
+    game = Game.new
+    game.game_loop
+  when 'load'
+    game = #call load method to load game class
+    if game
+      game.game_loop
+    else
+      puts "Load Falure, exiting"
+      exit
+    end
+  end
+
+  when 'exit'
+    puts 'Exiting game....'
+    exit
+
+  else
+    puts 'SELECTION ERROR EXITING'
+    exit
+    
+  end
+end
 
 class Game
 
@@ -14,7 +60,11 @@ class Game
     @wrong_guesses = []
   end
 
-  def start_game
+  def game_loop
+    puts "entering game"
+    puts "the current right answer is \"#{@correct_answer}\" meaning the dictionary is loaded!"
+    exit
+
     loop do
       # Game loop logic here
       break if game_over? # Add your exit condition
@@ -33,5 +83,6 @@ class Game
 end
 
 # Start the game
-game = Game.new
-game.start_game
+# start screen
+
+start_game
--- lib/input_validation.rb
+++ lib/input_validation.rb
@@ -1,3 +1,18 @@
 module InputValidation
-# validate user input
+
+  def self.opening_inputs(input)
+    acceptable_inputs = ['start', 'exit', 'load',]
+    current_input = input.downcase
+    loop do
+      if current_input.match(/(#{acceptable_inputs.join('|')})/)
+        return current_input
+      else
+        puts "Invaild entry, Please enter one of the following to continue, #{acceptable_inputs.join(" ").upcase}:"
+        current_input = gets.chomp
+      end
+    end
+  end
+
+
+
 end
